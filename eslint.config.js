67436a0a9195edbe13507840cd4be965be063be3
vladi2007 <ikonev05@mail.ru>
2026-05-13 16:27:57 +0500

    add eslint-prettier-config, eslint-plugin-prettier, add scripts in package.json

--- eslint.config.js
+++ eslint.config.js
@@ -4,16 +4,22 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
-
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins:{
+      prettier: eslintPluginPrettier,
+      
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+       eslintConfigPrettier,
     ],
     languageOptions: {
       globals: globals.browser,
@@ -22,6 +28,7 @@ export default defineConfig([
       "no-console":'warn',
       "no-duplicate-imports": 'error',
       'eqeqeq': ['error', 'always'],
+      'prettier/prettier': 'error',
     }
   },
 ])
